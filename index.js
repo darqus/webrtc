a20e55506ad659d1ce72dbe040876defe1b97f97
darqus <voladores@inbox.ru>
2024-10-16 01:27:59 +0300

    add hamburger-menu

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-const customFrameTextarea = document.getElementById('customFrame')
-const setCustomFrameButton = document.getElementById('setCustomFrame')
+const customFrameSrcInput = document.getElementById('customFrame');
+const setCustomFrameButton = document.getElementById('setCustomFrame');
+const hamburgerToggle = document.querySelector('.hamburger-button');
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+
+const hamburgerToggleIcons = {
+  open: '☰',
+  close: '✕',
+};
 
 setCustomFrameButton.addEventListener('click', () => {
-  const iframe = document.querySelector('iframe')
-  iframe.src = customFrameTextarea.value
-})
+  const iframe = document.querySelector('iframe');
+  iframe.src = customFrameSrcInput.value;
+});
+
+customFrameSrcInput.addEventListener('keyup', (event) => {
+  if (event.key === 'Enter') {
+    const iframe = document.querySelector('iframe');
+    iframe.src = customFrameSrcInput.value;
+  }
+});
+
+hamburgerToggle.addEventListener('click', () => {
+  hamburgerMenu.classList.toggle('open');
+  hamburgerToggle.innerText = hamburgerMenu.classList.contains('open')
+    ? hamburgerToggleIcons.close
+    : hamburgerToggleIcons.open;
+});
