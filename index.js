111f02953e9be95f3166c9fcd80e927cbb5b5c38
darqus <voladores@inbox.ru>
2024-10-16 01:28:51 +0300

    fix linting

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-const customFrameSrcInput = document.getElementById('customFrame');
-const setCustomFrameButton = document.getElementById('setCustomFrame');
-const hamburgerToggle = document.querySelector('.hamburger-button');
-const hamburgerMenu = document.querySelector('.hamburger-menu');
+const customFrameSrcInput = document.getElementById('customFrame')
+const setCustomFrameButton = document.getElementById('setCustomFrame')
+const hamburgerToggle = document.querySelector('.hamburger-button')
+const hamburgerMenu = document.querySelector('.hamburger-menu')
 
 const hamburgerToggleIcons = {
   open: '☰',
   close: '✕',
-};
+}
 
 setCustomFrameButton.addEventListener('click', () => {
-  const iframe = document.querySelector('iframe');
-  iframe.src = customFrameSrcInput.value;
-});
+  const iframe = document.querySelector('iframe')
+  iframe.src = customFrameSrcInput.value
+})
 
 customFrameSrcInput.addEventListener('keyup', (event) => {
   if (event.key === 'Enter') {
-    const iframe = document.querySelector('iframe');
-    iframe.src = customFrameSrcInput.value;
+    const iframe = document.querySelector('iframe')
+    iframe.src = customFrameSrcInput.value
   }
-});
+})
 
 hamburgerToggle.addEventListener('click', () => {
-  hamburgerMenu.classList.toggle('open');
+  hamburgerMenu.classList.toggle('open')
   hamburgerToggle.innerText = hamburgerMenu.classList.contains('open')
     ? hamburgerToggleIcons.close
-    : hamburgerToggleIcons.open;
-});
+    : hamburgerToggleIcons.open
+})
